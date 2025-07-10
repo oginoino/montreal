7ccf7fac09454f2b3e3ded0da86353f4bd3cc854
Gino Laranjeiras <gino.laranjeiras@onikel.com>
2025-07-10 17:31:27 -0300

    refactor(imports): update import paths to use relative paths instead of package

--- lib/utils/imports/common_libs.dart
+++ lib/utils/imports/common_libs.dart
@@ -48,10 +48,10 @@ export '../../providers/locale_provider.dart';
 // ROUTING
 // =============================================================================
 
-export 'package:montreal/route/app_route.dart';
+export '../../route/app_route.dart';
 export '../../route/handlers/handler_redirect.dart';
 export '../../route/routes.dart';
-export 'package:montreal/route/router.dart';
+export '../../route/router.dart';
 
 // =============================================================================
 // MODELS
@@ -63,10 +63,10 @@ export '../../model/locale_data.dart';
 // CONSTANTS
 // =============================================================================
 
-export 'package:montreal/utils/constants/app_constants.dart';
-export 'package:montreal/utils/constants/ui_constants.dart';
-export 'package:montreal/utils/constants/locale_constants.dart';
-export 'package:montreal/utils/constants/string_constants.dart';
+export '/utils/constants/app_constants.dart';
+export '/utils/constants/ui_constants.dart';
+export '/utils/constants/locale_constants.dart';
+export '/utils/constants/string_constants.dart';
 
 // =============================================================================
 // UI COMPONENTS
